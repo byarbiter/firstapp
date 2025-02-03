2eb1f5febbaa36d2cbe72d4e9f43ce5c917d598b
Arbiter <sakkunasuon2014@gmail.com>
2025-02-04 00:57:35 +0700

    week 8

--- index.php
+++ index.php
@@ -5,33 +5,33 @@ require_once('init/init.php');
 include('includes/header.inc.php');
 include('includes/navbar.inc.php');
 if (isset($_GET['page'])) {
-    $page = $_GET['page']; // about
+  $page = $_GET['page']; // about
 
-    $before_logIn_pages = ['login', 'register'];
-    $after_logIn_pages = [
-        'dashboard',
-        'user/home',
-        'user/create',
-        'user/update',
-        'user/delete',
+  $before_logIn_pages = ['login', 'register'];
+  $after_logIn_pages = [
+    'dashboard',
+    'user/home',
+    'user/create',
+    'user/update',
+    'user/delete',
 
-    ];
-    $admin_pages = ['user/home', 'user/create', 'user/update', 'user/delete'];
-    $user_page = [];
-    if (
-        $page === 'logout'  ||
-        (in_array($page, $before_logIn_pages) && !LoggedInUser()) ||
-        (in_array($page, $after_logIn_pages) && LoggedInUser())
-    ) {
-        if (in_array($page, $admin_pages) && !isAdmin()) {
-            header("Location: ./");
-        }
-        include('pages/' . $page . '.php');
-    } else {
-        header("Location: ./");
+  ];
+  $admin_pages = ['user/home', 'user/create', 'user/update', 'user/delete'];
+  $user_page = [];
+  if (
+    $page === 'logout'  ||
+    (in_array($page, $before_logIn_pages) && !LoggedInUser()) ||
+    (in_array($page, $after_logIn_pages) && LoggedInUser())
+  ) {
+    if (in_array($page, $admin_pages) && !isAdmin()) {
+      header("Location: ./");
     }
+    include('pages/' . $page . '.php');
+  } else {
+    header("Location: ./");
+  }
 } else {
-    include('pages/home.php');
+  include('pages/home.php');
 }
 include('includes/footer.inc.php');
 
