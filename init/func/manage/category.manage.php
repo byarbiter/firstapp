7971cb504e3083db9f9b3ab85f715e6fd8708cde
Arbiter <sakkunasuon2014@gmail.com>
2025-02-18 09:33:52 +0700

    week8

--- init/func/manage/category.manage.php
+++ init/func/manage/category.manage.php
@@ -3,18 +3,7 @@ function categorySlugExists($slug)
 {
   global $db;
   $query = $db->query("SELECT id_category FROM tbl_category WHERE slug = '$slug'");
-  //$db->close();
   if ($query->num_rows) {
-    return true;
-  }
-  return false;
-}
-function createCategory($name, $slug)
-{
-  global $db;
-  $query = $db->query("INSERT INTO tbl_category (name, slug) VALUES ('$name', '$slug')");
-  //$db->close();
-  if ($query) {
     return true;
   }
   return false;
@@ -29,6 +18,17 @@ function getCategories()
   }
   return null;
 }
+
+function createCategory($name, $slug)
+{
+  global $db;
+  $query = $db->prepare("INSERT INTO tbl_category (name,slug) VALUES ('$name','$slug')");
+  if ($query->execute()) {
+    return true;
+  }
+  return false;
+}
+
 function getCategoryByID($id)
 {
   global $db;
@@ -38,34 +38,17 @@ function getCategoryByID($id)
   }
   return null;
 }
+
 function updateCategory($id, $name, $slug)
 {
   global $db;
-
-  // if (empty($username)) {
-  //     $username_query = "";
-  // } else {
-  //     $username_query = ", username = '$username'";
-  // }
-
-  // $username_query = empty($username) ?  "" : ", username = '$username'";
-
-  // if (empty($passwd)) {
-  //     $passwd_query = "";
-  // } else {
-  //     $passwd_query = ", passwd = '$passwd'";
-  // }
-
-
-  // $db->query("UPDATE tbl_user SET user_label = '$user_label', username = '$username', passwd = '$passwd' WHERE id_user = '$id'");
-
   $db->query("UPDATE tbl_category SET name = '$name', slug = '$slug' WHERE id_category = '$id'");
-
   if ($db->affected_rows) {
-    return true;
+    return getCategoryByID($id);
   }
   return false;
 }
+
 function deleteCategory($id)
 {
   global $db;
--- pages/category/update.php
+++ pages/category/update.php
@@ -4,8 +4,9 @@ if (!isset($_GET['id']) || getCategoryByID($_GET['id']) === null) {
 }
 
 $manage_category = getCategoryByID($_GET['id']);
+
 $name_err = $slug_err = '';
-if (isset($_POST['name']) && isset($_POST['slug']) ) {
+if (isset($_POST['name']) && isset($_POST['slug'])) {
   $id_category = $_GET['id'];
   $name = $_POST['name'];
   $slug = $_POST['slug'];
@@ -14,38 +15,49 @@ if (isset($_POST['name']) && isset($_POST['slug']) ) {
     $name_err = 'Name is required';
   }
 
-  if (!empty($slug) && usernameExists($slug)) {
-    $slug_err = 'Slug already exists';
+  if (empty($slug)) {
+    $slug_err = 'Slug is required';
+  } else {
+    if ($slug !== $manage_category->slug && categorySlugExists($slug)) {
+      $slug_err = 'Slug already exists';
+    }
   }
 
   if (empty($name_err) && empty($slug_err)) {
-    if (updateCategory($id_category, $name, $slug)) {
-      header('Location: ./?page=category/home');
+    $manage_category = updateCategory($id_category, $name, $slug);
+    if ($manage_category !== false) {
+      echo '<div class="alert alert-success" role="alert">
+            Category Updated Successfully. <a href="./?page=category/home">Category page</a>
+            </div>';
+      $name_err = $slug_err = '';
+      unset($_POST['name']);
+      unset($_POST['slug']);
     } else {
       echo '<div class="alert alert-danger" role="alert">
-                    Can not update user!
-                    </div>';
+            Category Update Failed
+            </div>';
     }
   }
 }
-?>
 
-<form action="./?page=category/update&id=<?php echo $manage_category->id_category ?>" method="post" class="w-50 mx-auto">
-  <h1>Update Category ID: <?php echo $manage_category->id_category ?></h1>
+?>
+<form action="./?page=category/update&id=<?php echo $_GET['id'] ?>" method="post" class="w-50 mx-auto">
+  <h1>Update Category</h1>
   <div class="mb-3">
     <label for="name" class="form-label">Name</label>
-    <input type="text" name="name" class="form-control <?php echo $name_err !== '' ?  'is-invalid' : ' ' ?>" id="name" value="<?php echo isset($_POST['name']) ? $_POST['name'] : $manage_category->name ?>">
+    <input type="text" name="name" class="form-control <?php echo $name_err !== '' ?  'is-invalid' : ' ' ?>" value="<?php echo isset($_POST['name']) ? $_POST['name'] : $manage_category->name ?>">
     <div class="invalid-feedback">
       <?php echo $name_err ?>
     </div>
   </div>
   <div class="mb-3">
     <label for="slug" class="form-label">Slug</label>
-    <input type="text" name="slug" placeholder="(optional) input slug to update" class="form-control <?php echo $slug_err !== '' ?  'is-invalid' : ' ' ?>" id="slug" value="<?php echo isset($_POST['slug']) ? $_POST['slug'] : '' ?>">
+    <input type="text" name="slug" class="form-control <?php echo $slug_err !== '' ?  'is-invalid' : ' ' ?>" value="<?php echo isset($_POST['slug']) ? $_POST['slug'] : $manage_category->slug ?>">
     <div class="invalid-feedback">
       <?php echo $slug_err ?>
     </div>
   </div>
+
   <div class="d-flex justify-content-between">
     <a role="button" href="./?page=category/home" class="btn btn-secondary">Cancel</a>
     <button type="submit" class="btn btn-success">Update</button>
